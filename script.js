b9a62a2890b5eb286450f2b00a231cf71aeb0410
Dustin Brown <dustin.brown.k@gmail.com>
2021-06-07 16:57:05 -0400

    Started HTML layout.

--- script.js
+++ script.js
@@ -1,9 +1,20 @@
+
+let userLibrary = [];
+
+
 function Book(title, author, pages, read){
     this.title = title;
     this.author = author;
     this.pages = pages;
     this.read = read;
-    this.info = function() {
-        return console.log(`${title} by ${author}, ${pages} pages long, ${read}`);
-    }
+}
+Book.prototype.info = function() {
+    return console.log(`${title} by ${author}, ${pages} pages long, ${read}`);
+}
+function addToLibrary(title, author, pages, read) {
+    let addBook = new Book(title,author,pages,read);
+    return addBook;
+}
+function createBookDisplay(Book){
+    
 }
